456356943499de5c6be62017203b61d6595d2e1a
jiseok08 <heoji0912@gmail.com>
2025-01-31 19:45:55 +0900

    (2025.1.31) 템플릿

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,47 +2,25 @@
 
 using namespace std;
 
-class Vector2
+template <typename T>
+class List
 {
-private:
-	int x;
-	int y;
-public:
-	Vector2(int x,int y)
-	{
-		this->x = y;
-		this->y = y;
+	int size;
+	int index;
 
-	}
-	const int & X()
-	{
-		return x;
-	}
-
-	const int & Y()
-	{
-		return y;
-	}
+	T* pointer;
 };
-
-Vector2 operator + (const Vector2& clone)
-{
-	Vector2();
-
-	return X, Y;
-}
 
 int main()
 {
-#pragma region 연산자 오버로딩
+#pragma region 템플릿
+	// 데이터 형식에 의존하지 않고, 하나의 값이 여러 다른 데이터
+	// 형식을 가질 수 있는 기술에 중점을 두어 재사용성을 높일 수
+	// 있는 기능입니다.
 
-	Vector2 direction1(1,1);
-	Vector2 direction2(5,5);
-
-	Vector2 direction =  ;
+	List<int> intlist;
 
 #pragma endregion
-
 
 
 	return 0;
