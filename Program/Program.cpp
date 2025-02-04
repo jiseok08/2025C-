028b93f2b2d2387ca1c40eb9dc29a22bc5bb89b8
jiseok08 <heoji0912@gmail.com>
2025-02-04 19:45:34 +0900

    (2025.2.4) 예외 처리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -2,108 +2,84 @@
 
 using namespace std;
 
-template <typename T>
-class List
+class Packet
 {
 private:
-	int size;
-	int index;
 
-	T* pointer;
+	int errorCode = -9999;
+
 public:
-	List()
+	Packet()
 	{
-		size = 0;
-		index = 0;
-		pointer = nullptr;
+		cout << "Created Packet" << endl;
 	}
 
-	void Resize(int size)
+	const int & ErrorCode()
 	{
-		this->size = size;
-
-		pointer = new T[size];
-
-		for (int i = 0; i < size; i++)
-		{
-			pointer[i] = 0;
-		}
+		return errorCode;
 	}
 
-	void Add(T data)
+	~Packet()
 	{
-		if (index >= size)
-		{
-			cout << "Index Out of Range" << endl;
-		}
-		else
-		{
-			pointer[index++] = data;
-		}
+		cout << "Release Packet" << endl;
 	}
-
-	const T& operator [] (int index)
-	{
-		return pointer[index];
-	}
-
-	~List()
-	{
-		if (pointer != nullptr)
-		{
-			delete [] pointer;
-		}
-	}
-
 };
 
-template <typename T>
-bool Same(T argument, T parameter)
+class Resource
 {
-	return argument == parameter;
-}
+public:
+	Resource()
+	{
+		cout << "Created Resource" << endl;
+	}
 
-template<typename T>
-bool Same(char a[], char b[])
-{
-	return a == b;
-}
+	~Resource()
+	{
+		cout << "Release Resource" << endl;
+	}
+};
 
 int main()
 {
-#pragma region 템플릿
-	// 데이터 형식에 의존하지 않고, 하나의 값이 여러 다른 데이터
-	// 형식을 가질 수 있는 기술에 중점을 두어 재사용성을 높일 수
-	// 있는 기능입니다.
+#pragma region 스마트 포인터
+	// 포인터를 사용하는 동시에 자동으로 메모리 관리를
+	// 해주며 , 경계 확인과 같은 추가 기능을 제공하는 추상
+	// 포인터 형식 입니다.
 
-	// List<int> list;
+#pragma region unique pointer
+	// 특정한 객체를 하나의 스마트 포인터로만 가리킬 수
+	// 있도록 되어 있는 포인트입니다.
+
+	// unique_ptr<Packet> uniquePointer = make_unique<Packet>();
 	// 
-	// list.Resize(5);
+	// cout << "uniquePointer Error Code : " << uniquePointer->ErrorCode() << endl;
 	// 
-	// list.Add(1);
-	// list.Add(2);
-	// list.Add(3);
-	// list.Add(4);
-	// list.Add(5);
-	// list.Add(6);
+	// unique_ptr<Packet> uniqueReference = std::move(uniquePointer);
 	// 
-	// for (int i = 0; i < 5; i++)
-	// {
-	// 	cout << list[i] << endl;
-	// }
+	// cout << "uniquePointer Error Code : " << uniquePointer->ErrorCode() << endl;
+	// 
+	// cout << "uniqueReference Error Code : " << uniqueReference->ErrorCode() << endl;
 
 #pragma endregion
 
-#pragma region 템플릿 특수화
-	// 특정 자료형에 대해 다르게 처리하고 싶은 경우
-	// 특정한 자료형만 다른 형식으로 동작시키는 템플릿 기능입니다.
+#pragma region shared pointer
+	// 하나의 자원 객체를 여러 포인터 객체가 가리킬 수 있으며,
+	// 모든 포인터 객체가 자원 객체를 필요로 하지 않을 때 자원 객체를
+	// 해제하도록 설계되어있는 포인터입니다.
 
-	cout << Same('A','B') << endl;
-	cout << Same(15.7f,12.5f) << endl;
-	cout << Same(10, 10) << endl;
-	cout << Same("Apple", "Appla") << endl;
+	shared_ptr<Resource> oil = make_shared<Resource>();
+
+	cout << "Reference Count : " << oil.use_count() << endl;
+
+	shared_ptr<Resource> mineral = oil;
+
+	cout << "Reference Count : " << mineral.use_count() << endl;
 
 #pragma endregion
+
+
+#pragma endregion
+
 
 
 	return 0;
